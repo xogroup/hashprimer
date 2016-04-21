53934c80ecbf5e70322a5694dd3ed15541e7aa45
Rajendra Prashad <rprashad@xogrp.com>
2016-04-21 13:02:03 -0400

    npm install hashprimer --save

---
+++ example.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+
+var hashprimer = require('hashprimer');
+var hash = new hashprimer();
+
+var OS = {};
+hash.set(OS, 0.155, [ "AWS", "us-east-1a", "c4.xlarge", "cost" ]);
+hash.set(OS, null, [ "AWS", "us-east-1a", "c4.xlarge", "count" ], hash.increment);
+hash.set(OS, 10, [ "AWS", "us-east-1a", "c4.xlarge", "count" ], hash.increment);
+hash.set(OS, 'i-a1b2b3c4', [ "AWS", "us-east-1a", "c4.xlarge", "instances" ], hash.array);
+hash.set(OS, 'i-a4b3b2c1', [ "AWS", "us-east-1a", "c4.xlarge", "instances" ], hash.array);
+hash.set(OS, null, [ "AWS", "us-east-1a", "c4.xlarge", "monthly_charge" ], function(hash, value) {
+  return (OS.AWS['us-east-1a']['c4.xlarge'].cost * OS.AWS['us-east-1a']['c4.xlarge'].count * 24 * 30);
+});
+
+console.log(JSON.stringify(OS));
---
+++ hashprimer.js
@@ -0,0 +1,65 @@
+/*   copyright 2016 - Rajendra Prashad (rprashad@xogrp.com)
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+function hashprimer(){};
+
+hashprimer.prototype.set = function(hash, value, keys, func) {
+  while (keys.length > 0) {
+    var key = keys.shift();
+    if (!hash.hasOwnProperty(key)) {
+      hash[key] = {};
+    }
+    value = this.set(hash[key], value, keys, func);
+    if (!Array.isArray(value) && typeof(value) == 'object') {
+      hash[key] = Object.assign(hash[key], value);
+    }
+    else {
+      hash[key] = value;
+    }
+    return hash;
+  }
+  if (!func) {
+    return value;
+  }
+  else {
+    return func(hash, value);
+  }
+};
+
+hashprimer.prototype.increment = function(hash, value) {
+  if (typeof(hash) != 'number') {
+    hash = 0;
+  }
+
+  if (typeof(value) == 'number') {
+    hash += value;
+  }
+  else {
+    hash++;
+  }
+
+ return hash;
+};
+
+
+hashprimer.prototype.array = function(hash, value) {
+  if (!Array.isArray(hash)) {
+    hash = [];
+  }
+  hash.push(value);
+  return hash;
+};
+
+module.exports = hashprimer;
